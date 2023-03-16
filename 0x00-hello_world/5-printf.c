3795c563b74e917c56e930c93bc69bbcd59e634c
Fhumur <rakhunwanafhumulani@gmail.com>
2023-03-16 06:59:42 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by new line
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	printf("%s, but the outcome is a piece of art,\n", "With proper grammar");
+	return (0);
+}
+
