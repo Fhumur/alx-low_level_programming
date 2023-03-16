5f3fc853873c39516a247a4b5f30cc0cc91e0888
Fhumur <rakhunwanafhumulani@gmail.com>
2023-03-16 07:49:21 -0700

    taskos

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#define STDERR_FILENO 2
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19""
+ * Return: 1
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	return (1);
+}
+
