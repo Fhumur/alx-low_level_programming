56e8816d1a694469d1957f3bdcdfb0de94677037
Fhumur <rakhunwanafhumulani@gmail.com>
2023-03-17 12:06:15 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,21 +6,15 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	for (j = i + 1; j <= 9; j++)
+	putchar(i);
+	if (i != '9')
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
-		putchar(j + '0');
-		if (i < 8)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	putchar(',');
+	putchar(' ');
 	}
 	}
 
