dec1dfa3dcb3d47b481759fa94b84457b3eeac52
Fhumur <rakhunwanafhumulani@gmail.com>
2023-03-23 03:37:59 -0700

    5fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 9)
+			if (y > 9 && y <= 14)
 			{
-				_putchar((y / 20) + '0');
+				_putchar((y / 10) + '0');
 			}
-			_putchar((y % 20) + '0');
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
